146c303d9b12021cf073a699c32d77d6b0ec7273
Lui Liu <liulewie@gmail.com>
2016-05-20 00:46:45 -0700

    updated php script to support distinct counts

--- www/linechart_old.php
+++ www/linechart_old.php
@@ -1,5 +1,4 @@
 <?php 
-
 $labelmapping = array(
 	'id' 				=> 'ID',
 	'cid'				=> 'Course ID',
@@ -21,16 +20,13 @@ $labelmapping = array(
 	'waitlist' 			=> 'Waitlist Count',
 	'waitlistcap'		=> 'Waitlist Capacity',
 	'status'			=> 'Status');
-
 $typemapping = array(
 	'253' 				=> 'string',
 	'246'				=> 'number',
 	'3'					=> 'number', 
 	'11'				=> 'time');
-
 function databaseConnect(){
 	$desired_db = "TEST";
-
 	// Connect to mysql and check for errors
 	$db_connection = mysqli_connect("localhost", "lui", "", $desired_db);
 	if (!$db_connection) {
@@ -45,7 +41,6 @@ function databaseConnect(){
 function databaseClose($db_connection){
 	mysqli_close($db_connection);
 }
-
 function typemapping($domain){
 	if (strpos($domain, 'VARCHAR') !== FALSE){
 		return 'string';
@@ -58,7 +53,6 @@ function typemapping($domain){
 		exit;
 	}
 }
-
 function printCols(){
 	global $result;
 	global $result_xaxis;
@@ -68,33 +62,17 @@ function printCols(){
 	global $data_trend;
 	
 	echo "\"cols\":";
-
 	// print xaxis column
 	$id = $_GET['xaxis_attr'];
 	$label = $labelmapping[$id];
 	// $type = $typemapping[mysqli_fetch_field_direct($result_xaxis, 0)->type];
-
 	$listo = array();
 	$arr = array('id'=>$id,'label'=>$label,'pattern'=>'','type'=>'string');
 	$listo[] = $arr;
-
-	// for ($i = 0; $i < count($result_arr); $i++) {
- //        $element = $result_arr[$i];
-
-    foreach ($data_trend as $element) {
-
-        $label = "";
-		$i = count($element);
-		foreach($element as $cond){
-			$label = $label . "$cond->trend_name";
-			if($i != 1) 
-				$label = $label . ", ";
-			$i--;
-		}
-		$id = $label;
-
-		// $id = $data_trend[$i]->trend_attr . "_" . $i;
-		// $label = $data_trend[$i]->trend_name;
+	for ($i = 0; $i < count($result_arr); $i++) {
+        $element = $result_arr[$i];
+		$id = $data_trend[$i]->trend_attr . "_" . $i;
+		$label = $data_trend[$i]->trend_name;
 		// $type = $typemapping[mysqli_fetch_field_direct($element, 1)->type];
 		$arr = array('id'=>$id,'label'=>$label,'pattern'=>'','type'=>'number');
 		$listo[] = $arr;
@@ -102,37 +80,27 @@ function printCols(){
     echo json_encode($listo);
 	error_log(json_encode($listo));
 }
-
 function printRows(){
 	global $result;
 	global $result_arr;
 	global $result_xaxis;
 	echo  "\"rows\": ";
-
 	$listo = array();
-
 	// ensure that if different results have different number of rows, correct values will be matched
 	$last = array_fill(0,count($result_arr), 0);
-	$nonzero_only = $_GET['nonzero_only'];
-
 	while($x = mysqli_fetch_row($result_xaxis)){
 		
 		$list = array();
 		$list[] = array('v'=>$x[0], 'f'=>null);	// add xaxis information
-		$zero_count = 0;
-
 		for ($i=0; $i < count($result_arr); $i++) { 
-
 			// if there was not an unused previously fetched value
 			if ($last[$i] == 0) {
 				$row = mysqli_fetch_row($result_arr[$i]);
-
 				if ($x[0] == $row[0]){			// if the xaxis match
 					$list[] = array('v'=>$row[1], 'f'=>null);
 				} else {						// if the xaxis dont match
 					$last[$i] = array($row[0], $row[1]);
 					$list[] = array('v'=>0, 'f'=>null);
-					$zero_count++;
 				}
 			} else {		// if there was an unused previously fetched value
 				
@@ -141,27 +109,17 @@ function printRows(){
 					$last[$i] = 0;
 				} else {						// if the xaxis dont match
 					$list[] = array('v'=>0, 'f'=>null);
-					$zero_count++;
 				}
 			}
-
 		}
-		if($nonzero_only == 'false' || $zero_count != count($result_arr)){
-			// the inverse of $nonzero_only == 'true' && $zero_count == count($result_arr)
-			// since the above is the only condition when a row is ignored, the negated version is then the only time a row is inserted
-
-			$listo[] = array('c'=>$list);
-		}
+		$listo[] = array('c'=>$list);
 	}
-	// error_log(json_encode($listo));
+	error_log(json_encode($listo));
 	echo json_encode($listo);
-
 }
-
 // Construct SQL and query for results
 // $string = file_get_contents("sampleData.json");
 $db_connection = databaseConnect();
-
 // $query_str = "SELECT day, guardian, avenger, transformer FROM movie";
 // SELECT year, sum(enrollment) FROM Class GROUP BY year
 // yaxis_attr = enrollment
@@ -174,100 +132,53 @@ option 1
 		(SELECT year FROM Class GROUP BY year) AS X 
 		LEFT OUTTER JOIN
 		(SELECT year, sum(enrollment) FROM Class WHERE dept = 'Computer Science' GROUP BY year);
-
 	SELECT year, sum(enrollment) FROM
 		(SELECT year FROM Class GROUP BY year) AS X 
 		LEFT OUTTER JOIN
 		(SELECT year, sum(enrollment) FROM Class WHERE dept = 'Electrical Engineering' GROUP BY year);
-
 option 2
 	SELECT year FROM Class GROUP BY year
 	SELECT year, sum(enrollment) FROM Class WHERE dept = 'Computer Science' GROUP BY year
 	SELECT year, sum(enrollment) FROM Class WHERE dept = 'Electrical Engineering' GROUP BY year
-
 place into a result set, evaluate each one
-
 */
-
 $yaxis_attr = $_GET['yaxis_attr'];
 $yaxis_aggr = $_GET['yaxis_aggr'];
 $xaxis_attr = $_GET['xaxis_attr'];
 $data_group = $_GET['data_group'];
-
 // $string = "[{\"trend_attr\":\"dept\",\"trend_name\":\"Computer Science\"}, {\"trend_attr\":\"dept\",\"trend_name\":\"Electrical Engineering\"}, {\"trend_attr\":\"dept\",\"trend_name\":\"Environment\"}]";
 $data_trend = json_decode($data_group);
-
-
 $result;
 $result_arr;
 $result_xaxis;
-
 $xaxis_format = ($xaxis_attr == 'start' || $xaxis_attr == 'stop')? "DATE_FORMAT($xaxis_attr, '%H:%i')":$xaxis_attr;
-
-// $yaxis_attr_format = ($yaxis_attr == 'instructor')?"DISTINCT($yaxis_attr)":$yaxis_attr;
-
-$yaxis_format = ($yaxis_aggr == 'count')?"$yaxis_aggr(DISTINCT $yaxis_attr)":"$yaxis_aggr($yaxis_attr)";
-
-$query_str = "SELECT $xaxis_format
+$query_str = "SELECT $xaxis_attr
 			  FROM Class
 			  GROUP BY $xaxis_attr
 			  ORDER BY $xaxis_attr";
-
 $query_arr = array();
 foreach ($data_trend as $element) {
-
-	$condition = "";
-	if($element[0]->trend_attr != 'all'){
-		$condition = "WHERE ";
-		$i = count($element);
-		foreach($element as $cond){
-			$condition = $condition . "$cond->trend_attr = '$cond->trend_name'";
-			if($i != 1) 
-				$condition = $condition . " AND ";
-			$i--;
-		}
-	}
-	$query_arr[] = "SELECT $xaxis_format, $yaxis_format
-				  FROM Class
-				  $condition
-				  GROUP BY $xaxis_attr
-				  ORDER BY $xaxis_attr";
-	
-
-	/*
-	if($element[0]->trend_attr == 'all'){
-		$query_arr[] = "SELECT $xaxis_format, $yaxis_aggr($yaxis_attr)
+	if($element->trend_attr == 'all'){
+		$query_arr[] = "SELECT $xaxis_attr, $yaxis_aggr($yaxis_attr)
 					  FROM Class
 					  GROUP BY $xaxis_attr
 					  ORDER BY $xaxis_attr";
 	} else {
-		$condition = "";
-		$i = count($element);
-		foreach($element as $cond){
-			$condition = $condition . "$cond->trend_attr = '$cond->trend_name'";
-			if($i != 1) 
-				$condition = $condition . " AND ";
-			$i--;
-		}
-		$query_arr[] = "SELECT $xaxis_format, $yaxis_aggr($yaxis_attr)
+		$query_arr[] = "SELECT $xaxis_attr, $yaxis_aggr($yaxis_attr)
 					  FROM Class
-					  WHERE $condition
+					  WHERE $element->trend_attr = '$element->trend_name'
 					  GROUP BY $xaxis_attr
 					  ORDER BY $xaxis_attr";
 	}
-	*/
 }
-
 // Query the xaxis values and store the result variable
 $result_xaxis = mysqli_query($db_connection, $query_str);
-
 // Query the data values and store the result variable
 $result_arr = array();
 foreach ($query_arr as $element){
 	error_log($element);
 	$result_arr[] = mysqli_query($db_connection, $element);
 }
-
 // Print JSON object
 if (mysqli_num_rows($result_xaxis) > 0){
 	echo "{";
@@ -275,17 +186,13 @@ if (mysqli_num_rows($result_xaxis) > 0){
 	echo ",";
     printRows();
     echo "}";
-
 }else{
 	echo "Error: No results found";
 }
-
 // Free result variables 
 mysqli_free_result($result_xaxis);
 foreach ($result_arr as $element){
 	mysqli_free_result($element);
 }
-
 databaseClose($db_connection);
-
 ?>
